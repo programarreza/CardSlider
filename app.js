997129bd9ab1b3ddabcedcb84b9066ef25e8cfdc
programarreza <87148086+programarreza@users.noreply.github.com>
2023-06-07 16:25:35 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3 ,
+    spaceBetween: 50,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    // breakpoints: {
+    //   769: {
+    //     slidesPerView: 2,
+    //     slidesPerGroup: 2,
+    //   },
+    // },
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  });
